42faec9afc8b72f3ba0a64993085e789ec32e8f5
itmanB2ongithub <146710506+itmanB2ongithub@users.noreply.github.com>
2023-10-12 00:00:12 +0530

    Add files via upload
    Hiii

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleNavbar() {
+    var navbarLinks = document.getElementById('navbar-links');
+    navbarLinks.style.display = (navbarLinks.style.display === 'flex' || navbarLinks.style.display === '') ? 'none' : 'flex';
+  }
+  
+
+  
+var typed = new Typed(".auto",{
+  // strings:["Welcome to World Bharat"],
+  strings:["Welcome To Illuminati Society"],
+  typeSpeed:140,
+  backSpeed:120,
+  loop:true
+})
